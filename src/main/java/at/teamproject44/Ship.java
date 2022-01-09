0275ac0cfb1cc24c334a04fb859baacd8387980c
Safa <safa.sik@stud.fh-campuswien.ac.at>
2022-01-10 00:14:04 +0100

    Safa: small fixes in the Ship class and I removed my comment in the Main class.

--- src/main/java/at/teamproject44/Ship.java
+++ src/main/java/at/teamproject44/Ship.java
@@ -1,10 +1,16 @@
 package at.teamproject44;
 
+/* Die Klasse Ship erstellt die Schiffe und hat zusätzlich 2 Methoden die einen Treffer (Hit) ausführen und
+* die andere Klasse kontrolliert ob Schiff noch am Leben ist!*/
+
 public class Ship {
     private int type;
     private boolean vertical;
 
     private int health;
+
+    /*Konstruktor: erstellt ein Schiff mit den Attributen:
+    * Länge des Schiffes, vertikal oder horizontal und Leben von Schiff*/
 
     public Ship(int type, boolean vertical) {
         this.type = type;
@@ -24,15 +30,15 @@ public class Ship {
         return vertical;
     }
 
-    public void setVertical(boolean vertical) {
-        this.vertical = vertical;
-    }
+    /*Hit Klasse: Falls ein Schuss erfolgreich ein Schiff trifft, wird Leben von Schiff um 1 subtrahiert.*/
 
     public void hit() {
         if (health != 0) {
             health--;
         }
     }
+
+    /*isShipAlive Klasse: Die Klasse kontrolliert ob ein Schiff noch Leben hat oder schon vollständig gesunken ist.*/
 
     public boolean isShipAlive(){
         if (health > 0){
